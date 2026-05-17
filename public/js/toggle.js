dd96f704c6baed1313e1eecc96778f41b1ff4a79
MuhaiminIslamUtso <muhaiminislamutso@gmail.com>
2026-05-18 01:54:22 +0600

    toggle created

---
+++ public/js/toggle.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('.toggleBtn');
+
+buttons.forEach(button => {
+
+    button.addEventListener('click', function () {
+
+        const id = this.dataset.id;
+
+        fetch('../../api/toggle.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({id: id})
+        })
+        .then(response => response.json())
+        .then(data => {
+
+            if (data.ok) {
+                this.innerText = data.is_available ? 'Active' : 'Inactive';
+            }
+
+        });
+
+    });
+
+});
