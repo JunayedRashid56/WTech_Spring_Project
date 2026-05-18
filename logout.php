79ad1da494ccc65ac562dfc4c136e91155694cfc
JunayedRashid56 <junayed.isha37@gmail.com>
2026-05-18 12:07:26 +0600

    added logout.php

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+session_unset();
+
+session_destroy();
+
+setcookie(
+    "remember_token",
+    "",
+    time() - 3600,
+    "/"
+);
+
+header("Location: views/auth/login.php");
+
+exit;
+
+?>
